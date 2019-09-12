337921f34ade1587dc915b744a6450ad6b85faf3
filipe mazzon <filipe.mazzon.ribeiro@gmail.com>
2019-09-11 22:42:19 -0300

    :bug: put blockchainService in providers

--- src/components/network/network.module.ts
+++ src/components/network/network.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { NetworkController } from './network.controller';
 import { NetworkService } from './network.service';
+import { BlockchainService } from '../blockchain/blockchain.service';
 
 @Module({
   controllers: [NetworkController],
-  providers: [NetworkService],
+  providers: [
+    NetworkService,
+    BlockchainService,
+  ],
 })
-export class NetworkModule {}
+export class NetworkModule {
+}
