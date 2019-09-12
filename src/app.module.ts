ebf09256670e05053ae20d23af11b51b0785503a
filipe mazzon <filipe.mazzon.ribeiro@gmail.com>
2019-09-11 22:39:29 -0300

    :bug: fixing unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { NetworkModule } from './components/network/network.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [NetworkModule],
 })
-export class AppModule {}
+export class AppModule {
+}
