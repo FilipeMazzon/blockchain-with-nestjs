818fb1a83bef53727784a5f189dd610d5ab77f4f
filipe mazzon <filipe.mazzon.ribeiro@gmail.com>
2019-09-14 19:17:23 -0300

    :sparkles: websocket module

---
+++ src/components/websocket/websocket.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { WebsocketGateway } from './websocket.gateway';
+
+@Module({
+  providers: [WebsocketGateway],
+})
+export class WebsocketModule {
+}
